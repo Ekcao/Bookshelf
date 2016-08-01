371469d63b4becec45284b6d8d8fa924285ff33c
Eric <ekcao@cpp.edu>
2016-07-31 19:00:00 -0700

    React tools extension

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 var {app} = require('electron');
 var {BrowserWindow} = require('electron');
-var {ipcMain} = require('electron');
 
 var settings = require('./app/settings');
 
@@ -12,13 +11,11 @@ app.on('ready', function() {
     settings.saveSetting('test', 'test test');
     mainWindow = new BrowserWindow({height: 800, width: 1000});
 
+    // BrowserWindow.addDevToolsExtension('C:\\Users\\Eric\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\0.15.0_0');
+
     mainWindow.loadURL('file://' + __dirname + '/public/html/index.html');
 
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
-});
-
-ipcMain.on('read setting', function() {
-    mainWindow.webContents.send('setting read', settings.readSetting('test'));
 });
